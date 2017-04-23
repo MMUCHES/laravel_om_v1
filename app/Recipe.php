1a6b37004b54bb126222227e18bb93270d5a1f57
SEREE YOYYIM <sir.seree.im@gmail.com>
2017-04-23 23:31:01 +0700

    create model and migrateions :: db:seed

---
+++ app/Recipe.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    protected $fillable = ['name', 'description', 'image'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ingredients()
+    {
+        return $this->hasMany(RecipeIngredient::class);
+    }
+
+    public function directions()
+    {
+        return $this->hasMany(RecipeDirection);
+    }
+
+    public static function form()
+    {
+        return[
+            'name' => '',
+            'image' => '',
+            'description' => '',
+            'ingredients' => [
+                RecipeIngredient::form()
+            ],
+            'direction' => [
+                RecipeDirection::form(),
+                RecipeDirection::form()
+            ]
+        ];
+    }
+}
